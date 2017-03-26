aeb32af7020b108aa7bf09af91d315e32f91d751
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-03-26 11:08:41 +0530

    initial commit
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

---
+++ app/src/main/java/com/condingblocks/networkks/MainActivity.java
@@ -0,0 +1,82 @@
+package com.condingblocks.networkks;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnDOwnload;
+    EditText etUrl;
+    TextView tvData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnDOwnload = (Button) findViewById(R.id.btnDownload);
+        etUrl = (EditText) findViewById(R.id.etUrl);
+        tvData = (TextView) findViewById(R.id.tvData);
+
+        btnDOwnload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = etUrl.getText().toString();
+                DownloadData downloadData = new DownloadData();
+                downloadData.execute(url);
+            }
+        });
+
+    }
+
+    public class DownloadData extends AsyncTask<String, Void , String>{
+
+        @Override
+        protected String doInBackground(String... params) {
+            URL url = null;
+            try {
+                url = new URL(params[0]);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            HttpURLConnection httpURLConnection = null;
+            try {
+                 httpURLConnection = (HttpURLConnection) url.openConnection();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            int responseCode = 0;
+            try {
+                responseCode = httpURLConnection.getResponseCode();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+//            HttpURLConnection  httpURLConnection = (HttpURLConnection) urlConnection;
+
+
+            return String.valueOf(responseCode);
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+
+            tvData.setText(s);
+
+            super.onPostExecute(s);
+        }
+    }
+
+}
